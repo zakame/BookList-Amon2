febc6582a574624472992ed6051dc0265c107e2a
Zak B. Elep <zak.elep@yoyo.ph>
2013-08-01 20:38:31 +0800

    Add SQLite schema
    Add the tables for authors and books, and their lookup table.

--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -2,3 +2,17 @@ CREATE TABLE IF NOT EXISTS sessions (
     id           CHAR(72) PRIMARY KEY,
     session_data TEXT
 );
+CREATE TABLE IF NOT EXISTS book (
+    id    INTEGER PRIMARY KEY,
+    title TEXT
+);
+CREATE TABLE IF NOT EXISTS author (
+    id         INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name  TEXT
+);
+CREATE TABLE IF NOT EXISTS book_author (
+    book_id   INTEGER REFERENCES book(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    author_id INTEGER REFERENCES author(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (book_id, author_id)
+);
